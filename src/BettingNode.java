670c7cd0d98b52f2b90318edf7d083080a37f0a2
mjb <574381574@qq.com>
2017-05-22 16:34:04 +0800

    add init betting tree

--- src/BettingNode.java
+++ src/BettingNode.java
@@ -10,41 +10,49 @@ public class BettingNode {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public BettingNode init_betting_tree_r(GameState state, Game game, int[] num_entries_per_bucket) {
-		BettingNode node;
-		if (state.finished) {
-		//	Terminal Node
-			boolean showdown = (state.playerFolded[0] || state.playerFolded[1] ? false : true);
-			int[] fold_value = new int[2];
+	public static BettingNode initBettingTree(GameState state, int[] numEntriesPerBucket,int stage) {
+	    BettingNode	node;
+		if(state.isFinished() || state.getRoundIsOver(stage)){
+			// create termial node
+			boolean showdown = state.getPlayerFolded(0) || state.getPlayerFolded(1) ? false : true;
+			int[] foldValue = new int[2];
 			int money = -1;
-			// 自己fold时，money为对方的spent
-			for (int p = 0; p < 2; ++p) {
-		        if (state.playerFolded[p])
-		        {
-		          fold_value[p] = -1;
-		          money = state.spent[p];
-		        }
-		        else if (state.playerFolded[1-p])
-		        {
-		          fold_value[p] = 1;
-		          money = state.spent[1-p];
-		        }
-		        else
-		        {
-		          fold_value[p] = 0;
-		          money = state.spent[p];
-		        }
+			for (int p = 0; p < Game.MAX_PLAYERS; ++p) {
+				if (state.getPlayerFolded(p)) {
+					foldValue[p] = -1;
+					money = state.getSpent(p);
+				} else if (state.getPlayerFolded(1 - p)) {
+					foldValue[p] = 1;
+					money = state.getSpent(1 - p);
+				} else {
+					//no one had fold, so in showdown stage.
+					foldValue[p] = 0;
+					money = state.getSpent(p);
+				}
 			}
-			node = new TerminalNode(showdown, fold_value, money);
-		}//Terminal Node ifend
-		// Choice node
-		else {
-			// TODO 获取合法action数目
-			/* Choice node.  First, compute number of different allowable actions */
-			int num_choices = 3;//FIXME 暂时制订了action的数目
-			
-			
+			node = new TerminalNode(showdown, foldValue, money);
+			return node;
 		}
+		actionType[] actions = new actionType[Game.MAX_ABSTRACT_ACTION];
+		int numChoices = getActions(state, actions,stage);
+        int solnIdx=numEntriesPerBucket[0];
+        numEntriesPerBucket[0]+=numChoices;
+		BettingNode first_child = null;
+		BettingNode last_child = null;
+		for (int a = 0; a < numChoices; a++) {
+			GameState newState =(GameState)state.clone();
+			newState.doAction(actions[a]);
+			BettingNode child = initBettingTree(newState, numEntriesPerBucket,stage);
+			if (last_child != null) {
+				last_child.setSibling(child);
+			} else {
+				first_child = child;
+			}
+			last_child = child;
+		}
+		last_child.setSibling(null);
+		node = new InfoSetNode(solnIdx, numChoices,state.currentPlayer(), state.getRound(), first_child);
+		return node;
 	}
 	
 }
