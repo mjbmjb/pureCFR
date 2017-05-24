4f4a5527d34b9097cdbbe433282b1ba81f9eeb28
mjb <574381574@qq.com>
2017-05-24 12:40:39 +0800

    CardAbstraction 0.1

---
+++ src/CardAbstarction.java
@@ -0,0 +1,20 @@
+
+public class CardAbstarction implements Game {
+
+	public CardAbstarction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int numBucket(BettingNode node) {
+		return 0;
+	}
+	public int getBucket(BettingNode node, int[] boardCards) {
+		return 0;
+	}
+	public boolean canPrecomputeBucket(BettingNode node) {
+		return false; 
+	}
+	public void precomputeBuckets(Hand hand) {
+		
+	}
+}
