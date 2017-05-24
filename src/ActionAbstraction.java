82f0bfdf7b8499844d8124fbb1f3a075a11d063d
mjb <574381574@qq.com>
2017-05-24 12:42:19 +0800

    ActionAbstraction

---
+++ src/ActionAbstraction.java
@@ -0,0 +1,12 @@
+
+public class ActionAbstraction implements Game{
+
+	public ActionAbstraction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getActions(GameState state, ActionType[] action) {
+		return 0;
+	}
+	
+}
