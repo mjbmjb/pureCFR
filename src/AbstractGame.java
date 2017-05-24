12fdac6ece659fb968a965d3122706ce45f98a87
mjb <574381574@qq.com>
2017-05-24 12:38:21 +0800

    AbstractGame 0.1

--- src/AbstractGame.java
+++ src/AbstractGame.java
@@ -1,8 +1,15 @@
 
-public class AbstractGame {
-
+public class AbstractGame implements Game{
+	public BettingNode bettingTreeRoot;
+	public ActionAbstraction actionAbs;
+	public CardAbstarction cardAbs;
+	
 	public AbstractGame() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	protected void CountEntriesR(int[] numEntriesPerBucket, int[] TotalNumEntries) {
+		
 	}
 
 }
