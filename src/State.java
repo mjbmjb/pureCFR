5c0bee2a487bfca90a0147e4ff25fe1d2c678ce9
JaysenStark <838459914@qq.com>
2017-05-26 18:02:08 +0800

    add State.java MatchState.java

---
+++ src/State.java
@@ -0,0 +1,17 @@
+
+public class State {
+
+	public int handId;
+	public int maxSpent;
+	public int minNoLimitRaiseTo;
+	public int [] spent;
+	public Action [][] action;
+	public int [][] actingPlayer;
+	public int [] numActions;
+	public int round;
+	public boolean [] finished;
+	public boolean [] playerfolded;
+	public int [] boardCards;
+	public int [][] holeCards;
+	
+}
