bade87bf37160b61d76892967a1b4d4cf1f67a34
mjb <574381574@qq.com>
2017-06-06 16:08:09 +0800

    add precomputeBuckets[][]

--- src/Hand.java
+++ src/Hand.java
@@ -5,6 +5,8 @@ public class Hand implements IGame{
 	public int[][] hole_cards = new int[MAX_PLAYERS][MAX_HOLE_CARDS];
 	//投入的钱数
 	public int[] showdownValue = new int[2];
+	
+	public int[][] precomputedBuckets = new int[MAX_PLAYERS][MAX_ROUNDS];
 	public Hand() {
 		// TODO Auto-generated constructor stub
 	}
